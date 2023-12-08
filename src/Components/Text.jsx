e98a647d4f3aabfbe9fdfd16a17d0ed2f973361c
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-08 15:46:38 +0530

    create buttons

--- src/Components/Text.jsx
+++ src/Components/Text.jsx
@@ -5,7 +5,19 @@ import './Text.css'
 const Text = () => {
   return (
     <div>
-      
+      <div className="container">
+        <h2>Speech To Text Converter</h2>
+        <br />
+        <p>A React hook that coverts speech to Text</p>
+        <div className="main-content">
+
+        </div>
+        <div className="btn-style">
+            <button>Copy</button>
+            <button>Start Listening</button>
+            <button>Stop Listening</button>
+        </div>
+      </div>
     </div>
   )
 }
