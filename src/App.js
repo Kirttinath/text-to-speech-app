4936af3c5db57bfbcf9c63e03021dab2fb5fdd7d
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-08 15:52:56 +0530

    Some changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
-import Text from './Components/Text';
 
 function App() {
   return (
     <div className="App">
-      <Text/>
+      <div className="container">
+        <h2>Speech To Text Converter</h2>
+        <br />
+        <p>A React hook that coverts speech to Text</p>
+        <div className="main-content">
+
+        </div>
+        <div className="btn-style">
+            <button>Copy</button>
+            <button>Start Listening</button>
+            <button>Stop Listening</button>
+        </div>
+      </div>
     </div>
   );
 }
